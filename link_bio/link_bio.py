7fd1e524d9457f46ff34203a072ee14623b037cc
Deivisdev <deivispuertas0@gmail.com>
2024-04-02 20:23:51 -0500

    First deployment version

--- link_bio/link_bio.py
+++ link_bio/link_bio.py
@@ -8,8 +8,8 @@ from link_bio.styles.styles import Size as Size
 
 
 
-class State(rx.State):
-    pass
+#class State(rx.State):
+#    pass
 
 def index() -> rx.Component:
    return rx.chakra.box(   
